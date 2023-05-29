37581a1cf1821f9f387c7eac6d195dd7837ae1a0
Martti Malmi <sirius@iki.fi>
2023-05-29 13:09:08 +0300

    rm feed eose check

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -161,9 +161,7 @@ const Feed = ({ showDisplayAs, relays, filterOptions }: Props) => {
       window.removeEventListener('keydown', handleKeyDown);
     };
   }, [modalItemIndex]);
-
-  if (!eose) return null;
-
+  
   const renderFilterOptions = () => {
     return (
       <div className="flex mb-4 gap-2 mx-2 md:mx-0">
